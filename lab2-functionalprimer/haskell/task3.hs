11a829fafeefa98667e2b457f27581482954a922
safaa imran <safaa.imran7@gmail.com>
2024-01-30 11:54:35 +0000

    completed task 3

--- lab2-functionalprimer/haskell/task3.hs
+++ lab2-functionalprimer/haskell/task3.hs
@@ -1,7 +1,11 @@
 sgn x = if x < 0 then -1 else if x == 0 then 0 else 1
 
+sgn1 x  | x < 0 = -1
+        | x == 0 = 0
+        | x > 0 = 1
+
 main = do
     putStrLn "Please enter a number"
     input <- getLine 
     let x = (read input :: Int)
-    putStrLn( show (sgn (x)) )
+    putStrLn( show (sgn1 (x)) )
